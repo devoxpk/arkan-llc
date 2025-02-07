f3e0b4ab6cecc81a10dbb90dd9adb7f121b6a627
devoxpk <devoxpk@gmail.com>
2025-02-07 11:09:58 +0500

    'deploy'

--- src/app/components/video.js
+++ src/app/components/video.js
@@ -1,13 +1,14 @@
 "use client";
 import React from 'react';
 import '../css/poster.css';
-import Image from 'next/image'
+import dynamic from 'next/dynamic';
+
+const DynamicImage = dynamic(() => import('next/image'), { ssr: false });
 
 const Video = () => {
   return (
     <div className="poster-container" style={{minHeight:'0vh',backgroundColor:'white'}}>
-      <Image loading='lazy' width={0} height={0} style={{width:"100%",height:"auto"}} alt='Poster Video' src='/videoplayback.gif' />
-      
+      <DynamicImage loading='lazy' width={0} height={0} style={{width:"100%",height:"auto"}} alt='Poster Video' src='/videoplayback.gif' />
       <div className="poster-bottom-space"></div>
     </div>
   );
